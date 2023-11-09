5a7275b5e0e2a9656dc363fb64ddcb2380938d47
MadhavaKandala <149293303+MadhavaKandala@users.noreply.github.com>
2023-11-09 11:15:29 +0530

    #include <stdio.h>
    int main() {
        int n1, n2, max, lcm;
    
        scanf("%d %d", &n1, &n2);
    
       max = (n1 > n2) ? n1 : n2;
    
        while (1) {
            if (max % n1 == 0 && max % n2 == 0) {
                lcm = max;
                break;
            }
            ++max;
        }
    
        printf("%d", lcm);
        return 0;
    }

---
+++ Least_Common_Multiple.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, max, lcm;
+
+    scanf("%d %d", &n1, &n2);
+
+   max = (n1 > n2) ? n1 : n2;
+
+    while (1) {
+        if (max % n1 == 0 && max % n2 == 0) {
+            lcm = max;
+            break;
+        }
+        ++max;
+    }
+
+    printf("%d", lcm);
+    return 0;
+}
