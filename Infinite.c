9f01d6ef960ce73fa3f4c1bb56d38b61e7447598
MadhavaKandala <149293303+MadhavaKandala@users.noreply.github.com>
2023-11-09 10:20:42 +0530

    #include<stdio.h> #include<stdlib.h> int main(){     int n;
            while(n != -1){
                scanf("%d
    ", &n);
                if(n == -1){
                    break;
                }
                printf("%d
    ", n*n);
                
            }
        
    }

---
+++ Infinite.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n;
+    
+        while(n != -1){
+            scanf("%d
+", &n);
+            if(n == -1){
+                break;
+            }
+            printf("%d
+", n*n);
+            
+        }
+    
+}
