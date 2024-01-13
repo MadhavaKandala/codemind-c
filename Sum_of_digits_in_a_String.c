62654332cf41f2099fa30680b5f99eda22bf1e09
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2024-01-13 12:20:45 +0530

    #include<stdio.h> #include<string.h> int main(){     char n[100];     fgets(n,sizeof(n),stdin);     int sum=0;     for(int i = 0; i<strlen(n); i++){         if(n[i] >= '0' && n[i]<='9'){             sum+=n[i] - '0';         }     }     printf("%d", sum); }

---
+++ Sum_of_digits_in_a_String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char n[100];
+    fgets(n,sizeof(n),stdin);
+    int sum=0;
+    for(int i = 0; i<strlen(n); i++){
+        if(n[i] >= '0' && n[i]<='9'){
+            sum+=n[i] - '0';
+        }
+    }
+    printf("%d", sum);
+}
