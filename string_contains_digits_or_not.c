3c544628dce136dae86f7c2a693ff815cd1a9afd
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2024-01-13 12:56:52 +0530

    #include<stdio.h> #include<string.h>
    int main(){
        
        int t;
        scanf("%d", &t);
        for(int j = 0; j<t; j++){
            char n[100];
            scanf("%s",n);
            int count = 0;
             
            for(int i = 0; i < strlen(n); i++){
                if(n[i] >= '0' && n[i] <='9'){
                    count = 1;
                    break;
                }
            }
            printf("%s
    ",count == 0 ?"No" : "Yes");
        }
    }

---
+++ string_contains_digits_or_not.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    
+    int t;
+    scanf("%d", &t);
+    for(int j = 0; j<t; j++){
+        char n[100];
+        scanf("%s",n);
+        int count = 0;
+         
+        for(int i = 0; i < strlen(n); i++){
+            if(n[i] >= '0' && n[i] <='9'){
+                count = 1;
+                break;
+            }
+        }
+        printf("%s
+",count == 0 ?"No" : "Yes");
+    }
+}
