3a68d37fd8d8388050fab9e57b73b6ca0a29928f
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2024-01-15 23:35:20 +0530

    #include<stdio.h> #include<string.h>
    int main () {
        char str[2024];
        scanf("%[^
    ]", str);
        
        int a, b;
        scanf("%d %d", &a, &b);
        
        for (int i = a; i <= b; i++) {
            printf("%c", str[i]);
        }
        
    }

--- _Exact_Slice_Of_String_.c
+++ _Exact_Slice_Of_String_.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
 int main () {
-    char str[100];
-    fgets(str,sizeof(str),stdin);
-
+    char str[2024];
+    scanf("%[^
+]", str);
+    
     int a, b;
     scanf("%d %d", &a, &b);
-
+    
     for (int i = a; i <= b; i++) {
         printf("%c", str[i]);
     }
-
+    
 }
