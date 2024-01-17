22a1d2d49c0bdc251a290849a4e0bf87981d9284
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2024-01-17 19:56:12 +0530

    #include <stdio.h> #include <string.h>
    int main () {
        char str[2006];
        scanf("%s", str);
        int flag = 0;
        for (int i = 0; str[i] != NULL; i++) {
            int count = 0;
            for (int j = 0; str[j] != 0; j++) {
                if (str[i] == str[j] && i != j) {
                    count++;
                }
            }
            if (count == 0) {
                printf("%d", i);
                flag = 1;
                break;
            }
        }
        if (flag == 0) {
            printf("-1");
        }
    }

---
+++ First_Unique_Character_in_a_String.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    char str[2006];
+    scanf("%s", str);
+    int flag = 0;
+    for (int i = 0; str[i] != NULL; i++) {
+        int count = 0;
+        for (int j = 0; str[j] != 0; j++) {
+            if (str[i] == str[j] && i != j) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            printf("%d", i);
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0) {
+        printf("-1");
+    }
+}
