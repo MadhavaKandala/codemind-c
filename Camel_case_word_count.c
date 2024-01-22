787a6310cafd2fcc9816192f80c7deeb2c0b7e59
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2024-01-22 23:54:53 +0530

    #include<stdio.h> #include<ctype.h>
    int main(){
        char str[2006];
        scanf("%s", str);
        
        int count = 1;
        for(int i = 0; str[i] != NULL; i++){
            if(isupper(str[i]) && i != 0){
                count ++;
            }
        }
        printf("%d", count);
    }

--- Camel_case_word_count.c
+++ Camel_case_word_count.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<ctype.h>
 
- int main () {
-    char str[1001];
+int main(){
+    char str[2006];
     scanf("%s", str);
-     
+    
     int count = 1;
-    for (int i = 0; str[i] != 0; i++) {
-        if(isupper(str[i]) && i != 0) {
-            count++;
+    for(int i = 0; str[i] != NULL; i++){
+        if(isupper(str[i]) && i != 0){
+            count ++;
         }
     }
     printf("%d", count);
